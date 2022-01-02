5c4cc17d64f5542613baa00cc85629aad34986e4
jw5374 <jw5374@nyu.edu>
2022-01-02 13:54:53 -0500

    Euler 6

---
+++ EulerProj/E06.js
@@ -0,0 +1,12 @@
+function sumOfFirstNatural(n) {
+    let sum = (n * (n + 1)) / 2 // sum of arithmetic progression
+    return sum
+}
+
+function sumOfFirstSquares(n) {
+    let sum = (n * (n + 1) * ((2*n) + 1)) / 6 // sum of first squares
+    return sum
+}
+
+let answer = Math.pow(sumOfFirstNatural(100), 2) - sumOfFirstSquares(100)
+console.log(answer)
