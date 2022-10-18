b078746caa5339d2137b28b0358361c93a1bf828
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-10-18 11:56:52 -0400

    is same binary tree

---
+++ leet/sameTree.js
@@ -0,0 +1,31 @@
+const { treeGen, TreeNode } = require('../viewLib/forest.js')
+
+function isSameTree(p, q) {
+  if(p == null && q == null) {
+    return true
+  }
+  if((p == null && q != null) || (p != null && q == null)) {
+    return false
+  }
+  if(p.val != q.val) {
+    return false
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+}
+
+
+let vals = [1,2,1]
+let vals2 = [1,1,2]
+let nodes = []
+let nodes2 = []
+for(let val of vals) {
+  nodes.push(val == null ? null : new TreeNode(val))
+}
+for(let val of vals2) {
+  nodes2.push(val == null ? null : new TreeNode(val))
+}
+
+let tree = treeGen(nodes)
+let tree2 = treeGen(nodes2)
+console.log(isSameTree(tree, tree2))
+ 
