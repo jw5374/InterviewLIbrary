02142f935248f519c4f1f68f7a5bf42dfccd26ce
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-11-01 21:11:58 -0400

    find minimum in rotated sorted array

---
+++ leet/blind75/findMinRotated.js
@@ -0,0 +1,21 @@
+function findMin(nums) {
+  let [left, right] = [0, nums.length-1]
+  while(left <= right) {
+    let mid = Math.floor((left + right) / 2)
+    console.log(left, right, mid, nums[left], nums[right])
+    if(nums[left] <= nums[right]) {
+      return nums[left]
+    } else {
+      if(nums[mid-1] > nums[mid]) {
+        return nums[mid]
+      }
+      if(nums[left] <= nums[mid]) {
+        left = mid + 1
+      } else {
+        right = mid - 1
+      }
+    }
+  }
+}
+
+console.log(findMin([3,1,2]))
