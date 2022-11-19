a382849e13bf5ca6dbb8e733fa08f08275a543ca
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-11-19 14:54:02 -0500

    linear digit sum solution

--- leet/addDigitsUntilSingle.js
+++ leet/addDigitsUntilSingle.js
@@ -8,4 +8,23 @@ function addDigits(num) {
   return num % 9
 }
 
+/********* naive linear solution **********/
+function addDigitsNaive(num) {
+  let sum = basicDigitSum(num)
+  while(sum > 9) {
+    sum = basicDigitSum(sum)
+  }
+  return sum
+}
+
+function basicDigitSum(num) {
+  let sum = 0
+  while(num != 0) {
+    sum += num % 10
+    num = Math.floor(num / 10)
+  }
+  return sum
+}
+/*******************/
+
 console.log(addDigits(38))
