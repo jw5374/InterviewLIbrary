c09586715f792ff49440c6a8db16184b4ab21f06
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-12-09 16:50:42 -0500

    linear solution to power of three

--- leet/powerOfThree.py
+++ leet/powerOfThree.py
@@ -6,4 +6,11 @@ def isPowerOfThree(n: int) -> bool:
 	cblog = math.log(n, 3)  # LOGS ARE INACCURATE NOT MY FAULT >:(
 	return 3 ** math.ceil(cblog) == n or 3 ** math.floor(cblog) == n
 
-print(isPowerOfThree(1162261467))
+def linearPowerOfThree(n: int) -> bool:
+	while n > 1:
+		if n % 3 != 0:
+			return False
+		n = n // 3
+	return True
+
+print(linearPowerOfThree(1162261467))
