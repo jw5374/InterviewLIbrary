ac6da791a1f0db2e8401dd28c933a6b285471414
jw5374 <60762621+jw5374@users.noreply.github.com>
2023-02-26 11:20:59 -0500

    determine if array can be made into arithmetic progression

---
+++ leet/canMakeArithmeticProgression.py
@@ -0,0 +1,11 @@
+def canMakeArithmeticProgression(arr: list[int]) -> bool:
+    arr.sort()
+    diff = arr[1] - arr[0]
+    for i in range(1, len(arr)-1):
+        if arr[i+1] - arr[i] != diff:
+            return False
+    return True
+
+
+print(canMakeArithmeticProgression([3,5,1]))
+
