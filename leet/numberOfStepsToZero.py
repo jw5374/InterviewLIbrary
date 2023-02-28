50cd51ecc93ec940cf90f38948a6a280ae3db5d7
jw5374 <60762621+jw5374@users.noreply.github.com>
2023-02-28 16:23:00 -0500

    if even divide by 2, if odd subtract 1, count steps to 0

---
+++ leet/numberOfStepsToZero.py
@@ -0,0 +1,13 @@
+def numberOfSteps(num: int) -> int:
+    res = 0
+    while num > 0:
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num -= 1
+        res += 1
+    return res
+
+
+print(numberOfSteps(123))
+
