6dd32e3e114f84c45db2f1401772e32dcd75c4f0
jw5374 <60762621+jw5374@users.noreply.github.com>
2023-03-12 12:44:05 -0400

    find largest odd number substring

---
+++ leet/largestOddNumberString.py
@@ -0,0 +1,9 @@
+def largestOddNumber(num: str) -> str:
+    for i in range(len(num)-1, -1, -1):
+        if int(num[i]) % 2 != 0:
+            return num[:i+1]
+    return ""
+
+
+print(largestOddNumber("35427"))
+
