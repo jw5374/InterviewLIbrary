263df01f2b2927014f4a563ce8ab533e62bfe383
jw5374 <60762621+jw5374@users.noreply.github.com>
2023-03-14 16:44:48 -0400

    count number of string 'segments'

---
+++ leet/numberStringSegments.py
@@ -0,0 +1,6 @@
+def countSegments(s: str) -> int:
+    return len([x for x in s.split(" ") if len(x) > 0])
+
+
+print(countSegments("fg f dsfa    g fa gf a gf g gant eg oliteonah it o ot ghtnaiuon45 &G*9to9o78g9*OFo8fO*BFG 8GF898gb7BIKLBBILGU8h89o9POUHI789&689*(%^&$%^$#^#7b t 56$&564(&5T86t90 (&^7689 678 ^ &*(*(&^ %^& (%^& %^(% (%^ ((%^ (%  569 9t6 t6 79t67f  I   freaf876rg78f6tigbykl , , .,fhui erif"))
+
