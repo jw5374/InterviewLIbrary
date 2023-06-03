24b2a919aa0e9b8c0c363ae09ad4036668731650
jw5374 <60762621+jw5374@users.noreply.github.com>
2023-06-03 11:19:52 -0400

    given initial mass of a planet, find if all asteroids can be destroyed

---
+++ leet/destroyingAsteroids.py
@@ -0,0 +1,8 @@
+def asteroidsDestroyed(mass: int, asteroids: list[int]) -> bool:
+    asteroids.sort()
+    for asteroid in asteroids:
+        if mass >= asteroid:
+            mass += asteroid
+        else:
+            return False
+    return True
