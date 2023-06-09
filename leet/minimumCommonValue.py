f5f5aedf4bb900e749b5dc59ac175d5e250dd6a3
jw5374 <60762621+jw5374@users.noreply.github.com>
2023-06-09 17:01:35 -0400

    return minimal common value between 2 sorted arrays

---
+++ leet/minimumCommonValue.py
@@ -0,0 +1,10 @@
+def getCommon(nums1: list[int], nums2: list[int]) -> int:
+    [i, j] = [0, 0]
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] == nums2[j]:
+            return nums1[i]
+        if nums1[i] > nums2[j]:
+            j += 1
+        else:
+            i += 1
+    return -1
