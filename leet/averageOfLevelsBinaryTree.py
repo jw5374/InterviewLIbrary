1e860962ea8ebd4e6bf6682f6749a50eb421b5f4
jw5374 <60762621+jw5374@users.noreply.github.com>
2023-07-27 17:40:06 -0400

    calculate averages for each level in binary tree

---
+++ leet/averageOfLevelsBinaryTree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def averageOfLevels(root: TreeNode) -> list[float]:
+        nodeStack = [[root]]
+        res = []
+        while len(nodeStack) > 0:
+            currLev = nodeStack.pop()
+            currSum = 0
+            nextLev = []
+            for node in currLev:
+                currSum += node.val
+                if node.left is not None:
+                    nextLev.append(node.left)
+                if node.right is not None:
+                    nextLev.append(node.right)
+            if len(nextLev) > 0:
+                nodeStack.append(nextLev)
+            res.append(currSum / len(currLev))
+        return res
