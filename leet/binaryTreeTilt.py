1082e38bebe5c437301e9dd9e2f862137f626d51
jw5374 <60762621+jw5374@users.noreply.github.com>
2023-09-07 17:49:44 -0400

    find 'tilt' of binary tree

---
+++ leet/binaryTreeTilt.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def findTilt(root: TreeNode) -> int:
+    if root is None:
+        return 0
+    if root.left is None and root.right is None:
+        return 0
+    return abs(subtreeSum(root.left) - subtreeSum(root.right)) + findTilt(root.left) + findTilt(root.right)
+
+
+def subtreeSum(root: TreeNode) -> int:
+    if root is None:
+        return 0
+    if root.left is None and root.right is None:
+        return root.val
+    return root.val + subtreeSum(root.left) + subtreeSum(root.right)
