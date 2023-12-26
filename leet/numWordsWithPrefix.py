c297b2df3a18722b7534f3f0af93b0c9660e6b1e
jw5374 <60762621+jw5374@users.noreply.github.com>
2023-12-26 18:19:14 -0500

    find number of words in given array of strings that have a given prefix

---
+++ leet/numWordsWithPrefix.py
@@ -0,0 +1,8 @@
+def prefixCount(words: list[str], pref: str) -> int:
+	res = 0
+	for word in words:
+		if len(pref) > len(word):
+			continue
+		if word[0:len(pref)] == pref:
+			res += 1
+	return res
